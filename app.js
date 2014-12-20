5f01f29aeef2d8c42b63e072c507ef8e7ae2841a
hakatashi <hakatasiloving@gmail.com>
2014-12-21 00:56:41 +0900

    Change default port from 3000 to 10725

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ var csrfExclude = ['/url1', '/url2'];
  * Express configuration.
  */
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 10725);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(compress());
